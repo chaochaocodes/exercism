037d58f4ce21e9a729476751037fd1012312a5fd
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-08-30 23:04:06 -0400

    Subject (50 characters max) Problem (72 characters max) Solution Note
    [Ticket: X]

--- javascript/gigasecond/gigasecond.js
+++
@@ -1,8 +0,0 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const gigasecond = (startDate) => {
-  return new Date(Date.parse(startDate) + 1000000000000)
-};
